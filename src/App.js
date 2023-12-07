3bed7bb5206e6502962e15e21a3a08b305d6eca6
Jiyanshu Sharma <jiyanshusharma17@gmail.com>
2023-12-07 23:34:50 +0530

    First commit to Tic Tac toe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,125 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useState } from 'react'
+import './App.css'
+import X from './x.png'
+import O from './o.png'
+let data = ["" , "" , "" , "" , "" ,"" ,"" , "" ,""];
 
 function App() {
+ 
+  let [Count , setCount ] = useState(0);
+  let [lock , setLock] = useState(false);
+  let [first , setFirst] = useState("Result Pending ")
+  function handletoggle(e , num){
+    if(lock){
+      return 0;
+    }
+    if(Count%2 === 0){
+      e.target.innerHTML = `<img src=${X}>`;
+      data[num]="X";
+      setCount(Count+1);
+    }
+    else{
+      e.target.innerHTML = `<img src=${O}>`;
+      data[num]="O";
+      setCount(Count+1);
+    }
+    findWin();
+  }
+  const findWin =()=>{
+    console.log(data);
+    if(data[0] === data[1] && data[1] === data[2] && data[0] !== "" )
+    {
+      win(data[0]);
+    }
+    if(data[3] === data[4] && data[4] === data[5] && data[3] !== "" )
+    {
+      win(data[3]);
+    }
+    if(data[6] === data[7] && data[7] === data[8] && data[6] !== "" )
+    {
+      win(data[6]);
+    }
+    if(data[0] === data[4] && data[4] === data[8] && data[0] !== "")
+    {
+      win(data[0]);
+    }
+    if(data[2] === data[4] && data[4] === data[6] && data[2] !== "" )
+    {
+      win(data[2]);
+    }
+    if(data[0] === data[3] && data[3] === data[6] && data[0] !== "" )
+    { 
+      win(data[2]);
+    }
+    if(data[1] === data[4] && data[4] === data[7] && data[1] !== "" )
+    {
+      win(data[2]);
+    }
+    if(data[2] === data[5] && data[5] === data[8] && data[2] !== "" )
+    {
+      win(data[2]);
+    }
+    if (!data.includes("") && !lock) {
+      setFirst("It's a Tie!");
+      setLock(true);
+    }
+   
+  }
+ 
+  
+  const win =(winner)=>{
+    setLock(true);
+    if(winner === "X")
+    {
+      setFirst("Winner is : X")
+    }
+    else {
+      setFirst("Winner is : O")
+    }
+  }
+  const reset =()=>{
+    setLock(true);
+    setLock(false); 
+    setFirst("Result Pending "); 
+    setCount(0); 
+    let newData = ["", "", "", "", "", "", "", "", ""]; 
+    data = [...newData];
+    data.forEach((_,index)=>{
+      document.getElementById(`box-${index}`).innerHTML ="";
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Tic Tac Toe </h1>
+      <h1>{first}</h1>
+     
+     
+      <div className="board">
+        <div className="row">
+          <div className="Box" id ="box-0" onClick={(e)=>{handletoggle(e , 0)}}>A</div>
+          <div className="Box" id ="box-1" onClick={(e)=>{handletoggle(e , 1)}}>B</div>
+          <div className="Box" id ="box-2" onClick={(e)=>{handletoggle(e , 2)}}>C</div>
+        </div>
+        <div className="row">
+        <div className="Box" id ="box-3" onClick={(e)=>{handletoggle(e , 3)}}></div>
+          <div className="Box" id ="box-4" onClick={(e)=>{handletoggle(e , 4)}}></div>
+          <div className="Box" id ="box-5" onClick={(e)=>{handletoggle(e , 5)}}></div>
+        </div>
+        <div className="row">
+        <div className="Box" id ="box-6" onClick={(e)=>{handletoggle(e , 6)}}></div>
+          <div className="Box" id ="box-7" onClick={(e)=>{handletoggle(e , 7)}}></div>
+          <div className="Box" id ="box-8" onClick={(e)=>{handletoggle(e , 8)}}></div>
+        </div>
+
+      </div>
+      <button className='reset-btn' onClick={()=>{reset()}}>Reset </button>
+     
+     
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
